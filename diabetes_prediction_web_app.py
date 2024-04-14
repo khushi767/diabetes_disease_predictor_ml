8ccb1c05115bb2bb3a538a0ff27009f41060d5f4
khushi kumari <99918465+khushi767@users.noreply.github.com>
2024-04-15 04:36:42 +0530

    Update diabetes_prediction_web_app.py

--- diabetes_prediction_web_app.py
+++ diabetes_prediction_web_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open('D:/eye_anomaly/ml_project/trained_diabetes_model.sav', 'rb'))
+loaded_model = pickle.load(open('trained_diabetes_model.sav', 'rb'))
 
 
 # creating a function for Prediction
